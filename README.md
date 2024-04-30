52fec4f3362308239592281b77d4a55c741b65de
Logan Norman <lognorman20@gmail.com>
2024-05-01 11:04:17 +1200

    add more functions, change from FieldElement to u128

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Logan's STARK
+# Chef zkVM
 
-An implementation of a STARK following [this reference](https://aszepieniec.github.io/stark-anatomy/).
+A STARK-based zkVM targeting the [Chef programming language as a
+frontend](https://esolangs.org/wiki/Chef). This project contains an implementation of a STARK
+loosely follows [this
+reference](https://aszepieniec.github.io/stark-anatomy/).
--- src/multipolynomial.rs
+++ src/multipolynomial.rs
@@ -1,19 +1,66 @@
 use std::cmp::max;
 use std::collections::HashMap;
-use std::{iter, ops};
+use std::ops;
 
-use crate::algebra::FieldElement;
+use crate::algebra::{Field, FieldElement};
 
 #[derive(Debug, Clone)]
 pub struct MPolynomial {
     // vector of exponents : coefficients
-    dictionary: HashMap<Vec<FieldElement>, FieldElement>,
+    dictionary: HashMap<Vec<u128>, FieldElement>,
 }
 
 impl ops::Add for MPolynomial {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
+        let mut dictionary: HashMap<Vec<u128>, FieldElement> = HashMap::new();
+        let max_self = self
+            .dictionary
+            .keys()
+            .into_iter()
+            .map(|k| k.len())
+            .max()
+            .unwrap();
+        let max_rhs = rhs
+            .dictionary
+            .keys()
+            .into_iter()
+            .map(|k| k.len())
+            .max()
+            .unwrap();
+        let num_variables = max(max_self, max_rhs);
+
+        for (k, v) in self.dictionary.iter() {
+            let pad_zeroes: Vec<u128> = (0..num_variables - k.len())
+                .map(|_| 0)
+                .collect();
+            let new_key = [k.as_slice(), pad_zeroes.as_slice()].concat();
+            dictionary.insert(new_key, *v);
+        }
+
+        for (k, v) in rhs.dictionary.iter() {
+            let pad_zeroes: Vec<u128> = (0..num_variables - k.len())
+                .map(|_| 0)
+                .collect();
+            let new_key = &[k.as_slice(), pad_zeroes.as_slice()].concat();
+
+            if dictionary.contains_key(new_key) {
+                let new_val = dictionary[new_key] + *v;
+                dictionary.insert(new_key.to_owned(), new_val);
+            } else {
+                dictionary.insert(new_key.to_owned(), v.to_owned());
+            }
+        }
+
+        MPolynomial { dictionary }
+    }
+}
+
+impl ops::Mul for MPolynomial {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
         let mut dictionary = HashMap::new();
         let max_self = self
             .dictionary
@@ -31,24 +78,25 @@ impl ops::Add for MPolynomial {
             .unwrap();
         let num_variables = max(max_self, max_rhs);
 
-        for (k, v) in self.dictionary.into_iter() {
-            let pad_zeroes: Vec<FieldElement> = (0..num_variables - k.len())
-                .map(|_| k[0].field.zero())
-                .collect();
-            let new_key = [k.as_slice(), pad_zeroes.as_slice()].concat();
-            dictionary.insert(new_key, v);
-        }
+        for (k0, v0) in self.dictionary.iter() {
+            for (k1, v1) in rhs.dictionary.iter() {
+                let mut exponent: Vec<u128> =
+                    (0..num_variables).map(|_| 0).collect();
 
-        for (k, v) in rhs.dictionary.into_iter() {
-            let pad_zeroes: Vec<FieldElement> = (0..num_variables - k.len())
-                .map(|_| k[0].field.zero())
-                .collect();
-            let new_key = &[k.as_slice(), pad_zeroes.as_slice()].concat();
+                for k in 0..k0.len() {
+                    exponent[k] += k0[k];
+                }
+                for k in 0..k1.len() {
+                    exponent[k] += k1[k];
+                }
 
-            if dictionary.contains_key(new_key) {
-                dictionary.insert(new_key.to_owned(), dictionary[new_key] + v);
-            } else {
-                dictionary.insert(new_key.to_owned(), v);
+                if dictionary.contains_key(&exponent) {
+                    let new_val = dictionary[&exponent] + v0.to_owned() * v1.to_owned();
+                    dictionary.insert(exponent, new_val);
+                } else {
+                    let new_val = v0.to_owned() * v1.to_owned();
+                    dictionary.insert(exponent, new_val);
+                }
             }
         }
 
@@ -56,19 +104,11 @@ impl ops::Add for MPolynomial {
     }
 }
 
-impl ops::Mul for MPolynomial {
-    type Output = Self;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        todo!()
-    }
-}
-
 impl ops::Sub for MPolynomial {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        todo!()
+        self + (-rhs)
     }
 }
 
@@ -76,7 +116,12 @@ impl ops::Neg for MPolynomial {
     type Output = Self;
 
     fn neg(self) -> Self::Output {
-        todo!()
+        let mut dictionary = HashMap::new();
+        for (k, v) in self.dictionary.iter() {
+            dictionary.insert(k.to_owned(), -(v.to_owned()));
+        }
+
+        MPolynomial { dictionary }
     }
 }
 
@@ -84,12 +129,59 @@ impl ops::BitXor for MPolynomial {
     type Output = Self;
 
     fn bitxor(self, rhs: Self) -> Self::Output {
-        todo!()
+        if self.is_zero() {
+            MPolynomial {
+                dictionary: HashMap::new(),
+            }
+        } else {
+            let pair = self.dictionary.iter().next().unwrap();
+            let field = pair.1.field;
+            let num_variables = pair.0.len();
+            let exp: Vec<u128> = (0..num_variables).map(|_| 0).collect();
+
+            let mut dictionary = HashMap::new();
+            dictionary.insert(exp.to_owned(), field.one());
+            let mut acc = MPolynomial { dictionary };
+
+            for val in exp.iter().skip(2) {
+                acc = acc.clone() * acc;
+                if *val == 1 {
+                    acc = acc * self.clone();
+                }
+            }
+
+            acc
+        }
     }
 }
 
 impl MPolynomial {
-    pub fn new(self, dictionary: HashMap<Vec<FieldElement>, FieldElement>) -> Self {
+    pub fn new(self, dictionary: HashMap<Vec<u128>, FieldElement>) -> Self {
         MPolynomial { dictionary }
+    }
+
+    pub fn constant(&self, element: FieldElement) -> Self {
+        let mut dictionary = HashMap::new();
+        dictionary.insert(vec![0], element);
+
+        MPolynomial { dictionary }
+    }
+
+    pub fn is_zero(&self) -> bool {
+        if self.dictionary.is_empty() {
+            true
+        } else {
+            for v in self.dictionary.values() {
+                if v.is_zero() == false {
+                    return false
+                }
+            }
+
+            true
+        }
+    }
+
+    pub fn variables(&self, num_variables: u128, field: &Field) -> Vec<Self> {
+        todo!()
     }
 }
